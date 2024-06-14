78f85e7390fd1f4e3041dae088d2d7d3e0b0d559
ujjwa <ujjwa@Device>
2024-06-14 09:49:39 +0530

    first commit

---
+++ HelloWorld/src/edu/atria/b94java/inheritence/SingleInheritence.java
@@ -0,0 +1,14 @@
+package edu.atria.b94java.inheritence;
+
+public class SingleInheritence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee eOne = new Employee(789654123l,1212121212, "Indian", 121, "Rahul", "Front end developer", 40000.00);
+		
+		Citizen eTwo = new Employee (3201456, 13548975,"Indian",111,"Ravi", "Tester", 50000.00);
+		System.out.println(eTwo);
+			
+	}
+
+}
